320c68e7e0c4cf0209a1d9b6ce4ca14f6e10f15d
Nikola02Iliev <nikolacapitano@gmail.com>
2025-02-15 21:39:43 +0200

    Delete Frontend

--- WebAPICars/WebAPICars/obj/Debug/net8.0/WebAPICars.AssemblyInfo.cs
+++ WebAPICars/WebAPICars/obj/Debug/net8.0/WebAPICars.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPICars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c0dd8b740f4d056960e76d6293787e23a34d107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0671efdb80929896994ab37bebac77e435e42456")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPICars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPICars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
